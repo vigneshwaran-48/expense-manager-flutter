c492af3b2bf4e0a0e23b498a226107f4ee43998c
vigneshwaran48 <p3487260@gmail.com>
2025-04-16 21:50:27 +0530

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:expense_manager/config/router.dart';
+import 'package:expense_manager/firebase_options.dart';
 import 'package:expense_manager/navbar/navbar_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiBlocProvider(
       providers: [BlocProvider(create: (_) => NavbarCubit())],
