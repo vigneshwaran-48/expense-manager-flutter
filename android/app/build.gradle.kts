95d1c05ef64681f6959722d13c65e1c7daad1387
vigneshwaran48 <p3487260@gmail.com>
2025-04-19 13:22:00 +0530

    Mobile changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,8 @@ android {
         applicationId = "com.example.expense_manager"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
+        ndkVersion = "27.0.12077973"
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
